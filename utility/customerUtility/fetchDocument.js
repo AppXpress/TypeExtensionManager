4fde6398f31f43538cc234d11477ce89544ac632
Bhuvanesh Arasu <bhuvanesh.arasu@infor.com>
2019-10-15 15:22:08 -0400

    Letting user pick their objectUid if the query returns more than one object

--- utility/customerUtility/fetchDocument.js
+++ utility/customerUtility/fetchDocument.js
@@ -8,6 +8,9 @@ const {
     ENVIRONMENT,
     QUERY_URL,
     FETCH_URL,
+    UID: OBJECT_UID,
+    DOCUMENT_TYPE,
+    SAMPLE_REF_NUMBER,
   },
   GENERAL: {
     GET,
@@ -21,22 +24,44 @@ const {
     META_DATA,
     UID
   },
+  rl
 } = CONSTANTS
 
 export default (queriedObject, essential, options, poNumber) => {
   if (!poNumber) {
     return new Promise((resolve, reject) => {
+      console.log(JSON.stringify(queriedObject))
       if (essential[CUSTOMER][QUERY_URL] && essential[CUSTOMER][FETCH_URL] && queriedObject) {
         if (queriedObject && queriedObject[RESULT] && queriedObject[RESULT].length > 0) {
-          let objectUid = queriedObject[RESULT][0][DOCTYPES[`${essential[CUSTOMER][DOC_SHORT_FORM]}`][OBJECT_FIELDS][Object.keys(DOCTYPES[`${essential[CUSTOMER][DOC_SHORT_FORM]}`][OBJECT_FIELDS])]]
-          needle(GET, `${essential[CUSTOMER][FETCH_URL]}${objectUid}`, options)
-            .then((result) => {
-              let fetchedObject = result.body
-              resolve(fetchedObject)
+          let objectUidsList = [...new Set(queriedObject[RESULT].map(item => item[`${DOCTYPES[essential[CUSTOMER][DOC_SHORT_FORM]][OBJECT_UID]}`]))]
+          let objectUid = null
+          if(objectUidsList.length > 1){
+            let i = 0;
+            for(i; i < objectUidsList.length; i++){
+              console.log(`${i}.${objectUidsList[i]}`)
+            }
+            rl.question(`Looks like there are too many ${essential[CUSTOMER][DOCUMENT_TYPE]}'s with the same ${DOCTYPES[essential[CUSTOMER][DOC_SHORT_FORM]].SHORT_FORM}:#${essential[CUSTOMER][SAMPLE_REF_NUMBER]}. Please choose a UID:\n`, (uidOption) => {
+              objectUid = objectUidsList[uidOption]
+              needle(GET, `${essential[CUSTOMER][FETCH_URL]}${objectUid}`, options)
+                  .then((result) => {
+                    let fetchedObject = result.body
+                    resolve(fetchedObject)
+                  })
+                  .catch((err) => {
+                    reject(err)
+                  })
             })
-            .catch((err) => {
-              reject(err)
-            })
+          }else{
+            objectUid = queriedObject[RESULT][0][DOCTYPES[`${essential[CUSTOMER][DOC_SHORT_FORM]}`][OBJECT_FIELDS][Object.keys(DOCTYPES[`${essential[CUSTOMER][DOC_SHORT_FORM]}`][OBJECT_FIELDS])]]
+            needle(GET, `${essential[CUSTOMER][FETCH_URL]}${objectUid}`, options)
+                .then((result) => {
+                  let fetchedObject = result.body
+                  resolve(fetchedObject)
+                })
+                .catch((err) => {
+                  reject(err)
+                })
+          }
         }
       }
     })
